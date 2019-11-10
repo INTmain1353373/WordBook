619bd06aa9e92a2f5f2e5c71c9c9c53bc955297b
lihang <1670678504@qq.com>
2019-11-10 20:20:35 +0800

    导入数据库信息

--- app/src/main/java/com/example/wordbook/MainActivity.java
+++ app/src/main/java/com/example/wordbook/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.wordbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    public SQLitedb sqldb;
+    public static SQLiteDatabase database = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            sqldb = new SQLitedb(this);
+            sqldb.openDatabase();
+            sqldb.closeDatabase();
+            database = SQLiteDatabase.openOrCreateDatabase(SQLitedb.DB_PATH + "/" + SQLitedb.DB_NAME, null);
+        }else {
+            sqldb = new SQLitedb(this);
+            sqldb.openDatabase();
+            sqldb.closeDatabase();
+            database = SQLiteDatabase.openOrCreateDatabase(SQLitedb.DB_PATH + "/" + SQLitedb.DB_NAME, null);
+        }
     }
+
 }
